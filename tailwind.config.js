e7c0b3f3dc4823d1bf0a45780b42dbea5f35131a
Joel Maldonado Fernandez <106793712+JoelMaldonado@users.noreply.github.com>
2025-06-03 08:13:07 -0500

    feat: integrate Tailwind CSS and update layout
    - Added Tailwind CSS and Astro Tailwind integration to the project.
    - Removed the Welcome component and replaced it with a custom header and hero section in the index page.
    - Updated the Layout component to accept a title prop and changed the language to Spanish.
    - Created a new SVG asset for the logo.
    - Implemented a new footer section with contact information.
    - Configured Tailwind CSS with custom colors for the project.

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        p1: "#00CBC7",
+        s1: "#C6F103",
+        b1: "#141517",
+      },
+    },
+  },
+  plugins: [],
+};
